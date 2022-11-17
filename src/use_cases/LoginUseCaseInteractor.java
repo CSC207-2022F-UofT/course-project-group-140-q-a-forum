5edf88e801d8cdee0246a57418bb297d22afc8c5
cang_xue <qianjun.huang@mail.utoronto.ca>
2022-11-17 14:55:34 -0500

    LoginUseCaseSpecialFunction

--- src/use_cases/LoginUseCaseInteractor.java
+++ src/use_cases/LoginUseCaseInteractor.java
@@ -6,16 +6,18 @@ import java.util.ArrayList;
 
 
 public class LoginUseCaseInteractor {
-    final LoginDataInterface loginInterface;
+    private final LoginDataInterface loginInterface;
 
     public LoginUseCaseInteractor(LoginDataInterface loginInterface) {
         this.loginInterface = loginInterface;
     }
+
     /**
      * Take in User and Password, if verify successfully, return True
      * Otherwise, return False
-     * @param user password/
-     * @return boo
+     * @param user This is a User that we input
+     * @param password This is String that we input which represent the password
+     * @return true iff user is in our database and the password is the correct one
      */
     public boolean checkLogin(User user, String password){
         ArrayList<User> users = loginInterface.getData();
