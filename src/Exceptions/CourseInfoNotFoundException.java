f9dbd6a96051b22767526f783d35d4dab3e68826
blairyeung <loveblairsky@gmail.com>
2022-11-19 21:01:46 -0500

    Try

--- src/Exceptions/CourseInfoNotFoundException.java
+++ src/Exceptions/CourseInfoNotFoundException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 public class CourseInfoNotFoundException extends RuntimeException{
     public CourseInfoNotFoundException(String err_type, String code) {
+        // General exception
         super("The " + err_type +  " has not been found in the course " + code);
     }
 }
