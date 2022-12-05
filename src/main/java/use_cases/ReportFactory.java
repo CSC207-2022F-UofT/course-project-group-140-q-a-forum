e947baada2c7db2cf0efc59c2258a8713d369085
Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2022-12-04 22:04:46 -0500

    add factory

--- src/main/java/use_cases/ReportFactory.java
+++ src/main/java/use_cases/ReportFactory.java
@@ -8,6 +8,18 @@ import entities.ReportOnUser;
 import java.util.Map;
 
 public class ReportFactory {
+    /**
+     * Generate a Report
+     *
+     * @param reportInfo This is a Map that contains necessary information
+     *                   needed to register a report. The keys must be
+     *                   "Username", "ReportType", and "Content".
+     *                   The report type is identified by the value of "ReportType",
+     *                   0 for report on user,
+     *                   1 for report on post,
+     *                   2 for report on course.
+     * @return the Report generated.
+     */
     public Report getReport(Map<String, Object> reportInfo) {
         switch ((int) reportInfo.get("ReportType")) {
             case 0 -> {
